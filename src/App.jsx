674c3c950fe2d6eee3e41631076aa5f39cd4c75a
Sameer Alam <alamsameer738@gmail.com>
2023-01-28 22:52:01 +0530

    added search navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import { AiFillStar } from "react-icons/ai"
-import { FaSearch } from "react-icons/fa"
+import { AiFillStar, AiOutlinePlus, AiOutlineMinus } from "react-icons/ai"
+import { FaSearch, FaMinus, FaPlus } from "react-icons/fa"
 import { RoomProvider, useRoom } from './Store'
+
 import logo from "./assets/logo.svg"
 
-function LocationInput(){
-  const[isLocationopen,setLocationopen]=useState(false)
-  return <div>
-    <button className='col-start-1 col-end-4 p-4 border-r-2'>location</button>
-    {
-      isLocationopen&&<div>
-        <button>Helsinki,Finland</button>
-        <button>Turku,Finland</button>
-        <button>Vassa,Finland</button>
-        <button>oulu,Finland</button>
-      </div>
-    }
-  </div>
+function LocationInput() {
+
+
 }
 function MobileHeader() {
+  const [childCounter, setChildCounter] = useState(0)
+  const [adultCounter, setAdultCounter] = useState(0)
+  const [isLocationopen, setLocationopen] = useState(true)
   return (
     <div className=''>
       {/* logo */}
@@ -33,12 +27,39 @@ function MobileHeader() {
           <button className='col-start-4 col-end-8 border-r-2 text-gray-400'>Add guests</button>
           <button className='flex items-center justify-center  col-start-8 col-end-10 '><FaSearch className='text-red-400  text-xl' /></button>
         </div>
-        <div className='w-full border border-cyan-600'>
-          <div>
-
+        {/* dropdown in mobile  */}
+        <div className='w-full border border-cyan-600 absolute top-0 left-0 bottom-0 right-0 sm:bottom-auto sm:p-10 bg-white flex flex-col sm:grid sm:grid-cols-3 p-4'>
+          {/* location */}
+          <div className=' flex flex-col items-center'>
+            <button className='border border-black-600 w-4/5 rounded shadow-md font-medium p-2 mb-4'>Location</button>
+            <div className='w-3/5 p-4' onClick={() => { }}>
+              {
+                isLocationopen && <>
+                  <button className='block w-full mb-2'>Helsinki,Finland</button>
+                  <button className='block w-full mb-2'>Turku,Finland</button>
+                  <button className='block w-full mb-2'>Vassa,Finland</button>
+                  <button className='block w-full mb-2'>oulu,Finland</button>
+                </>
+              }
+            </div>
           </div>
-          <div>
-
+          {/* guest */}
+          <div className=' flex flex-col items-center'>
+            <button className='border border-black-600  w-4/5 rounded shadow-md font-medium p-2 mb-4'>Add guests</button>
+            <div className='flex gap-5 flex-col'>
+              <div className='flex gap-5 w-4/5'>
+                <p>Adults</p>
+                <div className='flex gap-5 rounded-2xl border-2 p-2 border-black '>
+                  <button onClick={() => { setAdultCounter(adultCounter - 1) }}><AiOutlineMinus /></button> <span className='font-bold text-lg block'>{adultCounter}</span><button onClick={() => { setAdultCounter(adultCounter + 1) }}><AiOutlinePlus /></button>
+                </div>
+              </div>
+              <div className='flex gap-7'>
+                <p>Child</p>
+                <div className='flex gap-5 rounded-2xl border-2 p-2 border-black'>
+                  <button onClick={() => { setChildCounter(childCounter - 1) }}><AiOutlineMinus /></button><span className='font-bold text-lg block'>{childCounter}</span><button onClick={() => { setChildCounter(childCounter + 1) }}><AiOutlinePlus /></button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
