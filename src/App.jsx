92d2e1ea52bfbd8a73054e9fbc1fc4f33c99aa3b
Sameer Alam <alamsameer738@gmail.com>
2023-02-10 11:47:22 +0530

    final winbnb-react website

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,12 @@ import { RoomProvider, useRoom } from './Store'
 
 import logo from "./assets/logo.svg"
 
-function LocationInput() {
-
-
-}
 function Navbar() {
   const [issearchBox, setSearchBox] = useState(true)
+  const [guestCounter, setGuestCounter] = useState(0)
+  const [locationSelected, setSelectedLocation] = useState("")
   const searchBoxRef = useRef()
-  const className = issearchBox ?   'scale-0 opacity-0 z-[-9999]':'scale-100'
+  const className = issearchBox ? 'scale-0 opacity-0 z-[-9999]' : 'scale-100'
   // handling the visibilty of the search box
   const handleSearchBox = (e) => {
     console.log(e);
@@ -25,26 +23,37 @@ function Navbar() {
   return (
     <div>
       {/* logo */}
-      <picture>
-        <img src={logo} alt="windbnb" />
-      </picture>
-      <div className='border mt-5 mb-7 grid grid-cols-9  rounded-2xl shadow-md ' ref={searchBoxRef} onClick={(e) => { handleSearchBox(e) }}>
-        <button className='col-start-1 col-end-4 p-4 border-r-2'>location</button>
-        <button className='col-start-4 col-end-8 border-r-2 text-gray-400'>Add guests</button>
-        <button className='flex items-center justify-center  col-start-8 col-end-10 '><FaSearch className='text-red-400  text-xl' /></button>
+      <div className='flex md:items-center flex-col md:flex-row justify-between '>
+        <picture className=' h-fit'>
+          <img src={logo} alt="windbnb" className='h-8' />
+        </picture>
+        <div className='border mt-5 mb-7 grid grid-cols-9 rounded-2xl shadow-md md:w-2/5' ref={searchBoxRef} onClick={(e) => { handleSearchBox(e) }}>
+          <button className='col-start-1 col-end-4 p-2 border-r-2'>location
+            <span className='block text-gray-400 text-sm'>{locationSelected}</span>
+          </button>
+          <button className='col-start-4 col-end-8 border-r-2 text-gray-400'>Add guests
+            <p>
+              <span className='text-gray-400 text-sm'>{guestCounter > 0 ? `${guestCounter} guests` : "guests"}</span>
+              {/* <span className='text-gray-400 text-sm'>{childCounter > 0 ? `${childCounter} child` : 'child'}</span> */}
+            </p>
+          </button>
+          <button id='search' className='flex items-center justify-center  col-start-8 col-end-10 '><FaSearch className='text-red-400  text-xl' /></button>
+        </div>
       </div>
 
-      <MobileHeader className={className} setSearchBox={setSearchBox} />
+      <MobileHeader className={className} setSearchBox={setSearchBox} guestCounter={guestCounter} setGuestCounter={setGuestCounter}
+        locationSelected={locationSelected} setSelectedLocation={setSelectedLocation}
+      />
 
     </div>
   )
 }
-function MobileHeader({ className,setSearchBox }) {
-  const [childCounter, setChildCounter] = useState(0)
-  const [adultCounter, setAdultCounter] = useState(0)
-  const [locationSelected, setSelectedLocation] = useState(" choose location")
-  const searchContainer=useRef(null)
+function MobileHeader({ className, setSearchBox, guestCounter, setGuestCounter, setSelectedLocation, locationSelected }) {
+  // const [guestCounter, setGuestCounter] = useState(0)
+  // const [locationSelected, setSelectedLocation] = useState("")
+  const searchContainer = useRef(null)
   const locationRef = useRef(null)
+  const { setSearch } = useRoom()
   const locationselected = (e) => {
     console.log(locationRef.current == e.target.parentElement)
     if (locationRef.current == e.target.parentElement) {
@@ -52,13 +61,13 @@ function MobileHeader({ className,setSearchBox }) {
     }
 
   }
-  const closeSearchBox=(e)=>{
-    if(searchContainer.current.id == e.target.id){
+  const closeSearchBox = (e) => {
+    if (searchContainer.current.id == e.target.id) {
       setSearchBox(true)
     }
   }
   return (
-    <div id='search-container' ref={searchContainer} className={`fixed bg-black bg-opacity-50 right-0 left-0 bottom-0 top-0  ${className} transition-all duration-500 ease-out`} onClick={(e)=>{closeSearchBox(e)}} >
+    <div id='search-container' ref={searchContainer} className={`fixed bg-black bg-opacity-50 right-0 left-0 bottom-0 top-0  ${className} transition-all duration-500 ease-out`} onClick={(e) => { closeSearchBox(e) }} >
 
       {/* dropdown in mobile  */}
       <div className='w-full border transition-transform tr h-full sm:h-auto  sm:bottom-auto sm:p-10 bg-white flex flex-col sm:grid sm:grid-cols-3 p-4'>
@@ -83,27 +92,35 @@ function MobileHeader({ className,setSearchBox }) {
         <div className=' flex flex-col items-center mb-4 '>
           <button className='border-2 border-black border-black-600 text-start w-4/5 rounded shadow-md font-medium p-2 mb-4'>Add guests
             <p>
-              <span className='text-gray-400 text-sm'>{adultCounter > 0 ? `${adultCounter} adults` : "adults"},</span>
-              <span className='text-gray-400 text-sm'>{childCounter > 0 ? `${childCounter} child` : 'child'}</span>
+              <span className='text-gray-400 text-sm'>{guestCounter > 0 ? `${guestCounter} guests` : "guests"}</span>
+              {/* <span className='text-gray-400 text-sm'>{childCounter > 0 ? `${childCounter} child` : 'child'}</span> */}
             </p>
           </button>
           <div className='flex gap-5 flex-col'>
             <div className='flex gap-5 w-4/5'>
-              <p>Adults</p>
+              <p>Guests</p>
               <div className='flex gap-5 rounded-2xl border-2 p-2 border-black '>
-                <button onClick={() => { setAdultCounter(adultCounter => adultCounter == 0 ? adultCounter : adultCounter - 1) }}><AiOutlineMinus /></button> <span className='font-bold text-lg block'>{adultCounter}</span><button onClick={() => { setAdultCounter(adultCounter + 1) }}><AiOutlinePlus /></button>
+                <button onClick={() => { setGuestCounter(guestCounter => guestCounter == 0 ? guestCounter : guestCounter - 1) }}><AiOutlineMinus /></button> <span className='font-bold text-lg block'>{guestCounter}</span><button onClick={() => { setGuestCounter(guestCounter + 1) }}><AiOutlinePlus /></button>
               </div>
             </div>
-            <div className='flex gap-7'>
+            {/* <div className='flex gap-7'>
               <p>Child</p>
               <div className='flex gap-5 rounded-2xl border-2 p-2 border-black'>
                 <button onClick={() => { setChildCounter(childCounter => childCounter == 0 ? childCounter : childCounter - 1) }}><AiOutlineMinus /></button><span className='font-bold text-lg block'>{childCounter}</span><button onClick={() => { setChildCounter(childCounter + 1) }}><AiOutlinePlus /></button>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
         <div className='justify-center flex mt-4 sm:mt-0'>
-          <button className='bg-red-400 text-white p-2.5 rounded-3xl min-w-fit h-fit w-2/4'>Search</button>
+          <button id='searchroom' onClick={() => {
+            let location = locationSelected.split(',')
+            let city = location[0]
+            console.log(city);
+            setSearch({
+              city,
+              guest: guestCounter
+            })
+          }} className=' border-2 border-black bg-red-400 text-white p-2.5 rounded-3xl min-w-fit h-fit w-2/4'>Search</button>
         </div>
       </div>
       {/*  show the search items */}
@@ -126,7 +143,7 @@ function Room({ room }) {
               <p className="room-type text-gray-400">{room.type}</p>
             </div>) : (<p className="room-type text-gray-400">{room.type}</p>)
           }
-
+          <p>{room.maxGuests}</p>
           <p className="room-rating font-bold   flex items-center gap-1 text-orange-400"><AiFillStar /> 4.4</p>
         </div>
         <div>
--- src/Store.jsx
+++ src/Store.jsx
@@ -1,16 +1,34 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext,useMemo ,  useCallback,useEffect, useState,useReducer } from "react";
 
 
  function useRoomSource(){
-    const[room,setRoomState]=useState([])
+    // const[room,setRoomState]=useState([])
+    const[{room,search},dispatch]=useReducer((state,action)=>{
+        switch(action.type){
+            case "setRoom":
+                return {...state,room:action.payload}
+            case "setSearch":
+                return {...state,search:action.payload}
+        }
+    },{room:[],search:{city:"",guest:9999}})
+    //  use of effect for fetching data
     useEffect(()=>{
         fetch("/stays.json")
         .then(data=>data.json())
         .then((stays=>{
-            setRoomState(stays)
+            dispatch({type:'setRoom',payload:stays})
         }))
     },[])
-    return {room}
+     const setSearch=useCallback((search)=>{
+        dispatch({type:"setSearch",payload:search})
+     },[])
+     const filteredRoom=useMemo(()=>{return room.filter((r)=>{
+        const {city,guest}=search
+        console.log({city,guest});
+        return r.maxGuests<=guest && r.city.includes(city)
+     })},[room,search]);
+     console.log("filteredRoom",filteredRoom);
+    return {room : filteredRoom,search,setSearch}
 }
 const RoomCtx=createContext([])
 export function useRoom(){
